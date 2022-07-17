f3fc73c1596b330875566763e135688642c26b3f
Styrmir Thorarinsson <StyrmirThorarins@users.noreply.github.com>
2022-07-17 02:50:21 +0000

    crypto list and interaction added, styling

--- components/CryptoList.tsx
+++ components/CryptoList.tsx
@@ -1,31 +1,41 @@
-const CryptoList = () => {
+const cryptoListFile = require('../lib/data/crypto-currencies.json');
 
-    const cryptoListFile = '../data/crypto-currencies.json'
+// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+// import { fa-solid, fa-circle-arrow-right } from '@fortawesome/pro-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleRight, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const CryptoList = (props: any) => {
+
+    const cryptoNameAcronym = props.name
 
     const getCryptoList = () => {
-        /*
-        const json = fetch(cryptoListFile)
-            .then(response => response.json())
-            .then(data => data)
-            */
+        let aCrypto = [];
 
-        const aCrypto = []
-        const json = JSON.parse(cryptoListFile)
-        for (let crypto in json) {
-            console.log(crypto)
-            aCrypto.push(crypto)
+        let n = 0
+        for (let item in cryptoListFile) {
+            aCrypto.push([item, cryptoListFile[item].name])
         }
+
+        return aCrypto;
+    }
+
+    const cryptoList = getCryptoList()
+
+    const addCryptoToGraph = (cryptoAcronym: string) => {
+        console.log('addCrypto', cryptoAcronym)
     }
 
 
-const cryptoList = getCryptoList()
-
-return (
-    <>
-        {/* cryptoList.map((crypto: any) => {} */}
-        {cryptoList}
-        [CryptoList]
-    </>
-)
+    return (
+        <div className='text-white'>
+            <ul className="items-list">
+                {cryptoList.map((item) => (
+                    <li key={item[0]}>{item[0]} - {item[1]} <FontAwesomeIcon icon={faArrowCircleRight} onClick={() => addCryptoToGraph(item[0])} /></li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 export default CryptoList
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,21 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
 
-  const [cryptoList, setCryptoList] = useState<string[]>([])
+  const [cryptoFree, setCryptoFree] = useState<string[]>([])
+  const [cryptoUsed, setCryptoUsed] = useState<string[]>([])
+  const [cryptoGraphList, setCryptoGraphList] = useState<string[]>([])
 
   useEffect(() => {
-    console.log('useEffect', cryptoList)
-  }), [cryptoList];
+    console.log('cryptoFree', cryptoFree)
+  }), [cryptoFree];
+
+  useEffect(() => {
+    console.log('cryptoUsed', cryptoUsed)
+  }), [cryptoUsed];
+
+  useEffect(() => {
+    console.log('cryptoGraphList', cryptoGraphList)
+  }), [cryptoGraphList];
 
   return (
     <div className={styles.container}>
@@ -28,16 +38,16 @@ const Home: NextPage = () => {
 
         <div className='object-center flex-1 p-4'>
           <div>
-            <p className='text-white text-lg'>Enter Crypto Name</p>
-            <input type="text" className='w-64 p-2 bg-gray-100 border-2 border-gray-200 rounded-lg' placeholder="Search" />
+            <p className='text-white text-lg'>Enter Crypto Name or Acronym</p>
+            <input type="text" className='w-64 p-2 my-4 bg-gray-100 border-2 border-gray-200 rounded-lg' placeholder="Search" />
           </div>
-          <div>
+          <div className='mt-2'>
             <CryptoList />
           </div>
         </div>
 
-        <div className='flex-1 p-4 w-1000' >
-          <CryptoGraph cryptoList={cryptoList} />
+        <div className='flex-1 p-4' style={{width: 800}} >
+          <CryptoGraph cryptoList={cryptoGraphList} />
         </div>
 
       </div>
