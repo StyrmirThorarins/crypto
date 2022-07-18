699e5b4c82fccec2574482a98f5c899ac4cbdef3
Styrmir Thorarinsson <StyrmirThorarins@users.noreply.github.com>
2022-07-18 12:01:44 +0000

    turned off debug for python

--- python/app.py
+++ python/app.py
@@ -11,44 +11,44 @@ from dotenv import load_dotenv
 import logging
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df_test = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig_test = px.bar(df_test, x="Fruit", y="Amount", color="City", barmode="group")
 
 # fetch data
-data = requests.get('https://min-api.cryptocompare.com/data/v2/histoday', params={'fsym': 'BTC', 'tsym': 'USD', 'limit': '10', 'api_key': os.getenv("CRYPTOCOMPARE_API_KEY")})
+def fetch_data(crypto_acronyms):
+    df = pd.DataFrame()
 
-df = pd.DataFrame(data.json()['Data']['Data'])
-print('df\n', df)
+    for acronym in crypto_acronyms:
+        data = requests.get('https://min-api.cryptocompare.com/data/v2/histoday', params={
+            'fsym': acronym, 'tsym': 'USD', 'limit': '10', 'api_key': os.getenv("CRYPTOCOMPARE_API_KEY")})
+        df = pd.DataFrame(data.json()['Data']['Data'])
 
-fig = px.line(df, x='time', y="open")
-# fig.show()
+    return px.line(df, x='time', y="open")
+
+# set up plotly graph, https://plotly.com/python/px-arguments/
+# https://community.plotly.com/t/announcing-plotly-py-4-8-plotly-express-support-for-wide-and-mixed-form-data-plus-a-pandas-backend/40048/10
+fig = px.line()
+fig = fetch_data(['BTC', 'ETH'])
 
 
+# create app html
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Crypto Demo with Python, Plotly and Flask'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Label('Select the crypto currencies you want to compare:'),
+    html.Br(),
+    dcc.Checklist(['BTC', 'ETH', 'USDT', 'USDC', 'BNB', 'BUSD', 'XRP', 'ADA', 'SOL', 'DOGE']),
 
-    html.Div(children=data.text),
-
+    # render plotly graph
     dcc.Graph(
-        id='example-graph',
+        id='crypto-graph',
         figure=fig
     )
-])
+], style={'padding': 10, 'flex': 1})
 
+
+# run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
